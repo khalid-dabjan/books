dd9b4eb669850916cbc89294ae42dd9711273ff1
ragheed <Z50@lenovo>
2017-01-04 12:24:09 +0200

    the Core work is done now with the Styling !

--- resources/views/books/addBook.blade.php
+++ resources/views/books/addBook.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.app')
 @section('content')
-<div class="container">
+<div class="container ">
     <form method='POST' 
-        action= '{{ $book->user_has_it?"/booksList/$book->id/delete":"/booksList/$book->id/add " }}' enctype="multipart/form-data"  >
-           {{ csrf_field() }} {{ $book->user_has_it?method_field("DELETE"):'' }}
-        <div>
+          action= '{{ $book->user_has_it?"/booksList/$book->id/delete":"/booksList/$book->id/add " }}' enctype="multipart/form-data"  >
+        {{ csrf_field() }} {{ $book->user_has_it?method_field("DELETE"):'' }}
+        <div class ="pull-right">
             <select name='status' class="input-sm">   
                 <option value='have'>I have this Book </option>
 
@@ -12,33 +12,26 @@
 
             </select>
         </div>
-        Book Title:
+        Title:{{ $book->title }}
         <div class='form-group'>       
-            {{ $book->title }}
-        </div>
-        Book Description:
-        <div class='form-group'>       
+            <img src="{{ $book->cover }}" >
+
+
             {{ $book->description }}
         </div>
-        Book Cover:
-        <div class='form-group'>       
-            {{ $book->cover }}
-        </div>
-        Book ISBN:
-        <div class='form-group'>       
-
-            {{ $book->isbn }}
-        </div>
-        Book Rating:
-        <div class='form-group'>       
-
-            {{ $book->rating }} 
-        </div>
+        <ul>
+            <li>
+                ISBN:{{ $book->isbn }}
+            </li>
+            <li>
+                Book Rating:{{ $book->rating }} 
+            </li>
+        </ul>
         <div>
             <button class='btn' type='submit' > "{{ $book->user_has_it?'Delete From List':'Add to list' }}"</button>
         </div>
     </form>
-    
+
 </div>
 
 @endsection
--- resources/views/books/booksList.blade.php
+++ resources/views/books/booksList.blade.php
@@ -2,43 +2,45 @@
 @section('content')
 
 <div class="table-responsive">
-    <table class="table table-striped">
+    <form enctype="multipart/form-data">
+        <table class="table table-striped">
 
-        <thead>
-            <tr>
-                <th>Book Title</th>
-                <th>Book Cover</th>
-                <th>Published at</th>
-                <th>Book Description</th>
-                <th>Book ISPN</th>
-                <th>Book Rating</th>
+            <thead>
+                <tr>
+                    <th>Book Title</th>
+                    <th>Book Cover</th>
+                    <th>Published at</th>
+                    <th>Book Description</th>
+                    <th>Book ISPN</th>
+                    <th>Book Rating</th>
 
-            </tr>
-        </thead>
+                </tr>
+            </thead>
 
-        <tbody>
-            @foreach($books as $book)
-            <tr>
-                <td>
-                    <a href="booksList/{{ $book->id }}/add">{{ $book->title }}
-                </td>
-                <td>
-                    {{ $book->cover }}
-                </td>
-                <td>
-                    {{ date('m:d: y',strtotime($book->created_at)) }}
-                </td>
-                <td>
-                    {{ $book->description }}
-                </td>
+            <tbody>
+                @foreach($books as $book)
+                <tr>
+                    <td>
+                        <a href="booksList/{{ $book->id }}/add">{{ $book->title }}
+                    </td>
+                    <td>
+                        <img src="{{ $book->cover }}" >
+                    </td>
+                    <td>
+                        {{ date('m:d: y',strtotime($book->created_at)) }}
+                    </td>
+                    <td>
+                        {{ $book->description }}
+                    </td>
 
-                <td>
-                    {{ $book->isbn }}
-                </td>
-                <td> 
-                    {{$book->rating}}
-                </td>
-            </tr>   
-        </tbody>    
-        @endforeach
-        @endsection
+                    <td>
+                        {{ $book->isbn }}
+                    </td>
+                    <td> 
+                        {{$book->rating}}
+                    </td>
+                </tr>   
+            </tbody>  
+    </form>
+    @endforeach
+    @endsection
